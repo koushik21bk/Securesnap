d26d94e7a97f879264b5fd47bf57b080c8bb1a8a
Manuel De La Rosa <manueldelarosa9722@gmail.com>
2023-07-28 17:58:24 -0500

    Created tests for user model. Updated navbar and user templates.

---
+++ photo_share/user/tests/test_models.py
@@ -0,0 +1,49 @@
+from django.test import TestCase
+from utils.tests.models import TestModel
+from user.models import User
+
+
+class TestUserModelFields(TestCase):
+    """This class tests field of the user model"""
+
+    # Test verbose names
+    def test_first_name_verbose_name(self):
+        label = TestModel.get_verbose_name(User, 'first_name')
+        self.assertEqual(label, 'first name')
+
+    def test_last_name_verbose_name(self):
+        label = TestModel.get_verbose_name(User, 'last_name')
+        self.assertEqual(label, 'last name')
+
+    def test_username_verbose_name(self):
+        label = TestModel.get_verbose_name(User, 'username')
+        self.assertEqual(label, 'username')
+
+    def test_email_verbose_name(self):
+        label = TestModel.get_verbose_name(User, 'email')
+        self.assertEqual(label, 'email address')
+
+    def test_password_verbose_name(self):
+        label = TestModel.get_verbose_name(User, 'password')
+        self.assertEqual(label, 'password')
+
+    # Test max length
+    def test_first_name_max_len(self):
+        max_len = TestModel.get_max_length(User, 'first_name')
+        self.assertEqual(max_len, 150)
+
+    def test_last_name_max_len(self):
+        max_len = TestModel.get_max_length(User, 'last_name')
+        self.assertEqual(max_len, 150)
+
+    def test_username_max_len(self):
+        max_len = TestModel.get_max_length(User, 'username')
+        self.assertEqual(max_len, 150)
+
+    def test_email_max_len(self):
+        max_len = TestModel.get_max_length(User, 'email')
+        self.assertEqual(max_len, 254)
+
+    def test_password_max_len(self):
+        max_len = TestModel.get_max_length(User, 'password')
+        self.assertEqual(max_len, 128)
---
+++ photo_share/utils/tests/models.py
@@ -0,0 +1,36 @@
+"""
+This module holds classes and functions to help with tests
+on models.
+"""
+from django.db.models import Model
+
+
+class TestModel:
+    @classmethod
+    def get_verbose_name(self, model: Model, field: str) -> str:
+        """
+        Use this method to get a model fields
+        verbose name
+
+        :param model: The model to use
+        :type model: Django model
+        :param field: The field you want to get the verbose name of
+        :type field: str
+        :rtype: str
+        :return: The model fields verbose name
+        """
+        return model._meta.get_field(field).verbose_name
+
+    @classmethod
+    def get_max_length(self, model: Model, field: str) -> int:
+        """
+        Use this method to get a model fields max length
+
+        :param model: The model to use
+        :type model: Django model
+        :param field: The field you want to get the max length for
+        :type field: str:
+        :rtype: int
+        :return: The maximum length a model field will accept
+        """
+        return model._meta.get_field(field).max_length
