497ea75f7682b4aead429aa53d7d0384bd7670ad
Manuel De La Rosa <manueldelarosa9722@gmail.com>
2023-07-30 15:21:20 -0500

    Updated the user profile template.
    Updated profile template to only show public images if the view is not
    the owner of the profile or to show all images if the view is the
    profile owner.

--- photo_share/user/views.py
+++ photo_share/user/views.py
@@ -4,6 +4,7 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from .models import User
 from .forms import RegistrationForm, LoginForm
+from photos.models import Image
 
 
 class RegisterView(CreateView):
@@ -32,3 +33,10 @@ class ProfileView(DetailView):
     model = User
     template_name = 'user/profile.html'
     context_object_name = 'profile_user'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Get public images from the owner of the profile
+        context['public_images'] = Image.objects.filter(
+            user=self.get_object()).filter(status=Image.Status.PUBLIC)
+        return context
